51e6928a340c3b13b6a22bde3346d86f22e5fe4c
ilia-io <likeapu@gmail.com>
2022-08-14 18:27:53 +0300

    feat: home component main content

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,11 +1,15 @@
-import React from 'react'
+import React from 'react';
 
-const Button = ({bgColor, color, size, text, borderRadius}) => {
+const Button = ({ bgColor, color, size, text, borderRadius }) => {
   return (
-    <button className={`text-${size} p-3 hover:drop-shadow-xl`} type='button' style={{backgroundColor: bgColor, color, borderRadius}}>
+    <button
+      className={`text-${size} p-3 hover:drop-shadow-xl`}
+      type="button"
+      style={{ backgroundColor: bgColor, color, borderRadius }}
+    >
       {text}
     </button>
-  )
-}
+  );
+};
 
-export default Button
+export default Button;
