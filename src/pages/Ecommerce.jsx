7eafc19ceb151eb9eb39e9c0e6c77ea745306ce3
ilia-io <likeapu@gmail.com>
2022-08-28 18:48:23 +0300

    feat: pie chart component

--- src/pages/Ecommerce.jsx
+++ src/pages/Ecommerce.jsx
@@ -48,7 +48,7 @@ const Ecommerce = () => {
               </button>
               <p className="mt-3">
                 <span className="text-lg font-semibold">{item.amount}</span>
-                <span className={`text-sm text-${item.pcColor} ml-2`}>
+                <span className={`text-sm ${item.pcColor} ml-2`}>
                   {item.percentage}
                 </span>
               </p>
